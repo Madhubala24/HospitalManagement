93719bc813dc88846a2f6fc7f8b5bfb3252b40ad
Madhubala24 <madhu24321@gmail.com>
2023-11-14 12:48:10 +0530

    optimise the code and add comment lines

--- src/appointment-management/AppointmentDetails.js
+++ src/appointment-management/AppointmentDetails.js
@@ -1,102 +1,9 @@
-// // AppointmentDetails.js without admin
-// import React, { useState, useEffect } from "react";
-// import "./appointmentDetails.css";
-// import { doctorData, patientData } from "../login/LoginData";
-
-// export default function AppointmentDetails() {
-//   const [appointments, setAppointments] = useState([]);
-//   const [filterBy, setFilterBy] = useState("day");
-//   const [filterValue, setFilterValue] = useState("");
-//   const [filteredAppointments, setFilteredAppointments] = useState([]);
-//   const [loggedInDoctor, setLoggedInDoctor] = useState(null);
-//   const [loggedInPatient, setLoggedInPatient] = useState(null);
-
-//   const getAppointmentsData = () => {
-//     const storedAppointments = JSON.parse(localStorage.getItem("appointments")) || [];
-//     setAppointments(storedAppointments);
-//     setFilteredAppointments(storedAppointments);
-//   };
-
-//   useEffect(() => {
-//     getAppointmentsData();
-//     const currentLoggedInDoctor = JSON.parse(localStorage.getItem("loggedInDoctor")) || null;
-//     const currentLoggedInPatient = JSON.parse(localStorage.getItem("loggedInPatient")) || null;
-    
-//     if (currentLoggedInDoctor) {
-//       setLoggedInDoctor(currentLoggedInDoctor);
-//     } else if (currentLoggedInPatient) {
-//       setLoggedInPatient(currentLoggedInPatient);
-//     }
-//   }, []);
-
-//   const handleFilter = () => {
-//     let filteredData = [...appointments];
-//     if (filterBy === "day") {
-//       filteredData = filteredData.filter((appointment) => appointment.day === filterValue);
-//     } else if (filterBy === "user") {
-//       filteredData = filteredData.filter((appointment) => appointment.patient === filterValue);
-//     } else if (filterBy === "doctor") {
-//       filteredData = filteredData.filter((appointment) => appointment.doctor === filterValue);
-//     }
-//     setFilteredAppointments(filteredData);
-//   };
-
-//   return (
-//     <div className="appointment-details-container">
-//       <h2>View Appointment Booking Details</h2>
-//       <div>
-//         <label>Filter By:</label>
-//         <select
-//           value={filterBy}
-//           onChange={(e) => setFilterBy(e.target.value)}
-//         >
-//           <option value="day">Day</option>
-//           <option value="user">User</option>
-//           <option value="doctor">Doctor</option>
-//         </select>
-//         <input
-//           type="text"
-//           value={filterValue}
-//           onChange={(e) => setFilterValue(e.target.value)}
-//         />
-//         <button onClick={handleFilter}>Filter</button>
-//       </div>
-
-//       <table className="appointment-table">
-//         <thead>
-//           <tr>
-//             <th>User</th>
-//             <th>Doctor</th>
-//             <th>Day</th>
-//             <th>Time</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {filteredAppointments.map((appointment, index) => (
-//             (appointment.doctor === loggedInDoctor?.doctorusername || appointment.patient === loggedInPatient?.username) && (
-//               <tr key={index}>
-//                 <td>{appointment.patient}</td>
-//                 <td>{appointment.doctor}</td>
-//                 <td>{appointment.day}</td>
-//                 <td>{appointment.time}</td>
-//               </tr>
-//             )
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-
-// AppointmentDetails.js
 import React, { useState, useEffect } from "react";
 import "./appointmentDetails.css";
-import { doctorData, patientData } from "../login/LoginData";
 import { useNavigate } from "react-router-dom";
 
 export default function AppointmentDetails() {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const [appointments, setAppointments] = useState([]);
   const [filterBy, setFilterBy] = useState("day");
   const [filterValue, setFilterValue] = useState("");
@@ -105,85 +12,104 @@ export default function AppointmentDetails() {
   const [loggedInPatient, setLoggedInPatient] = useState(null);
   const [isLoggedInAdmin, setIsLoggedInAdmin] = useState(false);
 
+  //fetch appointments data from localstrage
   const getAppointmentsData = () => {
-    const storedAppointments = JSON.parse(localStorage.getItem("appointments")) || [];
+    const storedAppointments =
+      JSON.parse(localStorage.getItem("appointments")) || [];
     setAppointments(storedAppointments);
     setFilteredAppointments(storedAppointments);
   };
 
   useEffect(() => {
-    getAppointmentsData();
-    const currentLoggedInDoctor = JSON.parse(localStorage.getItem("loggedInDoctor")) || null;
-    const currentLoggedInPatient = JSON.parse(localStorage.getItem("loggedInPatient")) || null;
-    const isLoggedInAdmin = JSON.parse(localStorage.getItem("isLoggedIn")) === "adminLog";
+    getAppointmentsData(); //init data on component
 
+    //fetch login doctor & patient data
+    const currentLoggedInDoctor =
+      JSON.parse(localStorage.getItem("loggedInDoctor")) || null;
+    const currentLoggedInPatient =
+      JSON.parse(localStorage.getItem("loggedInPatient")) || null;
+    const isLoggedInAdmin =
+      JSON.parse(localStorage.getItem("isLoggedIn")) === "adminLog";
     setLoggedInDoctor(currentLoggedInDoctor);
     setLoggedInPatient(currentLoggedInPatient);
     setIsLoggedInAdmin(isLoggedInAdmin);
   }, []);
 
+  //handlefilter action
   const handleFilter = () => {
     let filteredData = [...appointments];
     if (filterBy === "day") {
-      filteredData = filteredData.filter((appointment) => appointment.day === filterValue);
+      filteredData = filteredData.filter(
+        (appointment) => appointment.day === filterValue
+      );
     } else if (filterBy === "user") {
-      filteredData = filteredData.filter((appointment) => appointment.patient === filterValue);
+      filteredData = filteredData.filter(
+        (appointment) => appointment.patient === filterValue
+      );
     } else if (filterBy === "doctor") {
-      filteredData = filteredData.filter((appointment) => appointment.doctor === filterValue);
+      filteredData = filteredData.filter(
+        (appointment) => appointment.doctor === filterValue
+      );
     }
     setFilteredAppointments(filteredData);
   };
-  const  backtohome=()=>{
-    navigate('/home');
-  }
+
+  const backToHome = () => {
+    navigate("/home");
+  };
+
   return (
     <div className="appointmentview-body">
-    <div className="appointment-details-container">
-     <div className="back-button"> 
-     <button  onClick={backtohome}>Back</button>
-     </div>
-      <h2>View Appointment Booking Details</h2>
-      <div>
-        <label>Filter By:</label>
-        <select
-          value={filterBy}
-          onChange={(e) => setFilterBy(e.target.value)}
-        >
-          <option value="day">Day</option>
-          <option value="user">User</option>
-          <option value="doctor">Doctor</option>
-        </select>
-        <input
-          type="text"
-          value={filterValue}
-          onChange={(e) => setFilterValue(e.target.value)}
-        />
-        <button onClick={handleFilter}>Filter</button>
-      </div>
+      <div className="appointment-details-container">
+        <div className="back-button">
+          <button onClick={backToHome}>Back</button>
+        </div>
+        <h2>View Appointment Booking Details</h2>
+        <div>
+          <label>Filter By:</label>
+          <select
+            value={filterBy}
+            onChange={(e) => setFilterBy(e.target.value)}
+          >
+            <option value="day">Day</option>
+            <option value="user">User</option>
+            <option value="doctor">Doctor</option>
+          </select>
+          <input
+            type="text"
+            value={filterValue}
+            onChange={(e) => setFilterValue(e.target.value)}
+          />
+          <button onClick={handleFilter}>Filter</button>
+        </div>
 
-      <table className="appointment-table">
-        <thead>
-          <tr>
-            <th>User</th>
-            <th>Doctor</th>
-            <th>Day</th>
-            <th>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAppointments.map((appointment, index) => (
-            (isLoggedInAdmin || appointment.doctor === loggedInDoctor?.doctorusername || appointment.patient === loggedInPatient?.username) && (
-              <tr key={index}>
-                <td>{appointment.patient}</td>
-                <td>{appointment.doctor}</td>
-                <td>{appointment.day}</td>
-                <td>{appointment.time}</td>
-              </tr>
-            )
-          ))}
-        </tbody>
-      </table>
-    </div>
+        <table className="appointment-table">
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Doctor</th>
+              <th>Day</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredAppointments.map(
+              (appointment, index) =>
+                //Display appointments based on user role and filter criteria
+                (isLoggedInAdmin ||
+                  appointment.doctor === loggedInDoctor?.doctorusername ||
+                  appointment.patient === loggedInPatient?.username) && (
+                  <tr key={index}>
+                    <td>{appointment.patient}</td>
+                    <td>{appointment.doctor}</td>
+                    <td>{appointment.day}</td>
+                    <td>{appointment.time}</td>
+                  </tr>
+                )
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- src/doctor-management/EditForm.js
+++ src/doctor-management/EditForm.js
@@ -2,159 +2,180 @@ import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 export default function EditForm() {
+  // Retrieve the index parameter from the URL
   const { index } = useParams();
+  // Retrieve doctorData from local storage
   const doctorData = JSON.parse(localStorage.getItem("doctorData") || "[]");
+  // Set initial form data based on the selected doctors index
   const [formData, setFormData] = useState(doctorData[index]);
+  // Initialize navigate function for navigation
   const navigate = useNavigate();
 
+  // Handle input changes in the form
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  // Handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Update doctorData with the edited form data
     doctorData[index] = formData;
+
+    // Save the updated doctorData to local storage
     localStorage.setItem("doctorData", JSON.stringify(doctorData));
-    navigate("/doctor-management"); // Redirect to the details page after editing
+
+    // after editing
+    navigate("/doctor-management");
   };
-  const backtohome=()=>{
-    navigate('/doctor-management');
-  }
+
+  // Navigate back to the doctor management page
+  const backtohome = () => {
+    navigate("/doctor-management");
+  };
 
   return (
-    <div className="container">
-      <button onClick={backtohome}> Back </button>
-      <h1>Edit Doctor Information</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-element">
-          <label htmlFor="name" className="label">
-            Name:
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="input"
-            required
-          />
-        </div>
-        <div className="form-element">
-          <label className="radio-label">Gender:</label>
-          <label className="radio-label">
+    <div className="register-body">
+      <div className="registration-container">
+        <button onClick={backtohome} className="back-button">
+          Back
+        </button>
+        <h1>Edit Doctor Information</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-element">
+            <label htmlFor="name" className="label">
+              Name:
+            </label>
             <input
-              type="radio"
-              name="gender"
-              value="Male"
-              checked={formData.gender === "Male"}
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
               onChange={handleChange}
+              className="input"
+              required
             />
-            Male
-          </label>
-          <label className="radio-label">
+          </div>
+
+          <div className="form-element">
+            <label className="label">Gender:</label>
+            <div className="radio-group">
+              <label className="radio-label">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Male"
+                  checked={formData.gender === "Male"}
+                  onChange={handleChange}
+                />
+                Male
+              </label>
+              <label className="radio-label">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Female"
+                  checked={formData.gender === "Female"}
+                  onChange={handleChange}
+                />
+                Female
+              </label>
+              <label className="radio-label">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Other"
+                  checked={formData.gender === "Other"}
+                  onChange={handleChange}
+                />
+                Other
+              </label>
+            </div>
+          </div>
+
+          <div className="form-element">
+            <label htmlFor="department" className="label">
+              Department:
+            </label>
             <input
-              type="radio"
-              name="gender"
-              value="Female"
-              checked={formData.gender === "Female"}
+              type="text"
+              id="department"
+              name="department"
+              value={formData.department}
               onChange={handleChange}
+              className="input"
+              required
             />
-            Female
-          </label>
-          <label className="radio-label">
+          </div>
+
+          <div className="form-element">
+            <label htmlFor="experience" className="label">
+              Experience (in years):
+            </label>
             <input
-              type="radio"
-              name="gender"
-              value="Other"
-              checked={formData.gender === "Other"}
+              type="number"
+              id="experience"
+              name="experience"
+              value={formData.experience}
               onChange={handleChange}
+              className="input"
+              required
             />
-            Other
-          </label>
-        </div>
+          </div>
 
-        <div className="form-element">
-          <label htmlFor="department" className="label">
-            Department:
-          </label>
-          <input
-            type="text"
-            id="department"
-            name="department"
-            value={formData.department}
-            onChange={handleChange}
-            className="input"
-            required
-          />
-        </div>
+          <div className="form-element">
+            <label htmlFor="availability" className="label">
+              Availability:
+            </label>
+            <select
+              id="availability"
+              name="availability"
+              value={formData.availability}
+              onChange={handleChange}
+              className="input"
+              required
+            >
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Friday">Friday</option>
+            </select>
+          </div>
+          <div className="form-element">
+            <label htmlFor="fromTime" className="label">
+              From:
+            </label>
+            <input
+              type="time"
+              id="fromTime"
+              name="fromTime"
+              value={formData.fromTime}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+            <label htmlFor="toTime" className="label">
+              To:
+            </label>
+            <input
+              type="time"
+              id="toTime"
+              name="toTime"
+              value={formData.toTime}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+          </div>
 
-        <div className="form-element">
-          <label htmlFor="experience" className="label">
-            Experience (in years):
-          </label>
-          <input
-            type="number"
-            id="experience"
-            name="experience"
-            value={formData.experience}
-            onChange={handleChange}
-            className="input"
-            required
-          />
-        </div>
-
-        <div className="form-element">
-          <label htmlFor="availability" className="label">
-            Availability:
-          </label>
-          <select
-            id="availability"
-            name="availability"
-            value={formData.availability}
-            onChange={handleChange}
-            className="input"
-            required
-          >
-            <option value="Monday">Monday</option>
-            <option value="Tuesday">Tuesday</option>
-            <option value="Wednesday">Wednesday</option>
-            <option value="Thursday">Thursday</option>
-            <option value="Friday">Friday</option>
-          </select>
-        </div>
-
-        <div className="form-element">
-          <label htmlFor="fromTime" className="label">
-            From:
-          </label>
-          <input
-            type="time"
-            id="fromTime"
-            name="fromTime"
-            value={formData.fromTime}
-            onChange={handleChange}
-            className="input"
-            required
-          />
-          <label htmlFor="toTime" className="label">
-            To:
-          </label>
-          <input
-            type="time"
-            id="toTime"
-            name="toTime"
-            value={formData.toTime}
-            onChange={handleChange}
-            className="input"
-            required
-          />
-        </div>
-        <div className="form-element">
-          <input type="submit" value="Save" className="submit-button" />
-        </div>
-      </form>
+          <div className="form-element">
+            <input type="submit" value="Save" className="submit-button" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
